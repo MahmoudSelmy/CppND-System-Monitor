48dd547216cc63083330bea937127d52c2350b5f
Mahmoud Selmy <mahmoudselmy06@gmail.com>
2020-05-31 17:16:13 +0200

    + parse process username

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -289,7 +289,32 @@ string LinuxParser::Uid(int pid)
 
 // TODO: Read and return the user associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::User(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::User(int pid) 
+{ 
+  string line;
+
+  string username;
+  string password;
+  string uid;
+
+  std::ifstream filestream(kPasswordPath);
+  if (filestream.is_open()) 
+  {
+    while (std::getline(filestream, line)) 
+    {
+      std::replace(line.begin(), line.end(), ':', ' ');
+      std::istringstream linestream(line);
+      while (linestream >> username >> password >> uid) 
+      {
+        if (uid == LinuxParser::Uid(pid)) 
+        {
+          return username;
+        }
+      }
+    }
+  }
+  return string(); 
+}
 
 // TODO: Read and return the uptime of a process
 // REMOVE: [[maybe_unused]] once you define the function
