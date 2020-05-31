3feb8170023f99c746880ec6ce43a7d73f4baae3
Mahmoud Selmy <mahmoudselmy06@gmail.com>
2020-05-31 15:40:19 +0200

    + parse process command

--- src/linux_parser.cpp
+++ src/linux_parser.cpp
@@ -237,7 +237,18 @@ int LinuxParser::RunningProcesses()
 
 // TODO: Read and return the command associated with a process
 // REMOVE: [[maybe_unused]] once you define the function
-string LinuxParser::Command(int pid[[maybe_unused]]) { return string(); }
+string LinuxParser::Command(int pid) 
+{ 
+  string line;
+  string process_path = kProcDirectory + to_string(pid) + kCmdlineFilename;
+  std::ifstream filestream(process_path);
+  if (filestream.is_open()) 
+  {
+    std::getline(filestream, line);
+    return line;
+  }
+  return string(); 
+}
 
 // TODO: Read and return the memory used by a process
 // REMOVE: [[maybe_unused]] once you define the function
